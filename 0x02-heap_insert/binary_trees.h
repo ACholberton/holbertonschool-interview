a1ca4f11e00dbccf538d2acfd841492fa41c9420
ACholberton <1103@holbertonschool.com>
2020-08-04 07:09:51 +0000

    added missing funciton to header file

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,8 +19,9 @@ struct binary_tree_s
   struct binary_tree_s *left;
   struct binary_tree_s *right;
 };
-
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
